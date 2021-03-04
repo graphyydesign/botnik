7a974bd87d10460e14227d4fda23147cd10e75b1
graphyydesign <79003319+graphyydesign@users.noreply.github.com>
2021-03-04 09:39:42 +0530

    Add files via upload

---
+++ people.php
@@ -0,0 +1,322 @@
+<?php include "header.php" ?>
+
+    <section class="people-page">
+        <div class="container">
+            <div class="main">
+
+                <div class="current-section">
+                    <div class="row">
+                        <div class="col-md-4">
+                           <h2>People</h2>
+                        </div>
+                        <div class="col-md-8 text-right d-flex align-items-center justify-content-end">
+                            <div class="btn-grp mt-0">
+                                <button class="mr-sm-0 mr-3">
+                                    
+                                    <!-- Dark-Mode-Svg -->
+                                    <img src="assets/images/filter.svg" class="mr-3 dark-svg">
+                                    <!-- Dark-Mode-Svg -->
+                                    
+                                    <!-- Light-Mode-Svg -->
+                                    <img src="assets/images/filter-light.svg" class="mr-3 light-svg">
+                                    <!-- Light-Mode-Svg -->
+                                    
+                                    Filter
+                                </button>
+                                <button class="ml-3">
+                                    Add people
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="team">
+                                <div class="row">
+                                    <div class="col-xl-3">
+                                        <div class="team-head">
+                                            Team participation <b>70%</b>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-9">
+                                        <div class="progress-bar-team">
+                                        </div>
+                                            <div class="colors">
+                                            <div class="color1">
+                                                <div class="btn-bar" data-toggle="tooltip" data-placement="top" title="25%">
+                                                  
+                                                </div>
+                                            </div>
+                                            <div class="color2">
+                                            <div class="btn-bar" data-toggle="tooltip" data-placement="top" title="20%">
+                                                  
+                                                </div>
+                                            </div>
+                                            <div class="color3">
+                                             <div class="btn-bar" data-toggle="tooltip" data-placement="top" title="25%">
+                                                  
+                                                </div>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <h6 class="small-head">
+                        You
+                    </h6>
+                    <div class="coll-grp multiple-layer">
+                        
+                        <div href="#"  class="main-coll">
+                            <div class="row align-items-center">
+                                <div class="col-auto">
+                                <div class="user-detail">
+                                    <div class="media align-items-center">
+                                          <img class="" src="assets/images/tim.png" alt="Generic placeholder image">
+                                          <div class="media-body">
+                                            <h5 class="mt-0">Tim Chilcott</h5>
+                                            <p>tim.chilcott@botnik.com</p>
+                                          </div>
+                                        </div>
+                                </div>
+                                </div>
+                                <div class="col-lg-auto col-12">
+                                <div class="people-progress">
+                                    <span>81%</span>
+                                    <div class="progress-people-team">
+                                     <div class="progress-people-main" style="width:81%"></div>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="col-xl-auto col-lg-12 col-sm-12">
+                                    <ul class="ul-people d-flex align-items-center p-0 m-0">
+                                        <li class="favorites-btn">
+                                            
+                                             <div class="add">
+                                               <img src="assets/images/star.svg" class="dark-svg">
+                                            <img src="assets/images/star-dark.svg" class="light-svg">
+                                            <span class="ml-1">Add to favorites</span>
+                                            </div>
+                                            <div class="remove">
+                                                <img src="assets/images/star-fill.svg" class="dark-svg">
+                                                <img src="assets/images/star-fill-dark.svg" class="light-svg">
+                                                <span class="ml-1">Remove favorites</span>
+                                            </div>
+                                           
+                                        </li>
+                                        
+                                        <li class="notification-btn">
+                                            <div class="mute">
+                                                <img src="assets/images/notifications-off.svg" class="dark-svg">
+                                                <img src="assets/images/notifications-dark.svg" class="light-svg">
+                                                <span class="ml-1">Mute notification</span>
+                                            </div>
+                                            <div class="unmute">
+                                                <img src="assets/images/notifications.svg" class="dark-svg">
+                                                <img src="assets/images/notifications-fill-dark.svg" class="light-svg">
+                                                <span class="ml-1">Unmute notification</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                       
+        
+
+                </div>
+                <div class="past-section">
+                    <h6 class="small-head mb-2">
+                        Team
+                    </h6>
+                    <div class="coll-grp multiple-layer" style="margin-top:32px;">
+                        
+                        <div href="#"  class="main-coll">
+                            <div class="row align-items-center">
+                                <div class="col-auto">
+                                <div class="user-detail">
+                                    <div class="media align-items-center">
+                                          <img class="" src="assets/images/user-02.png" alt="Generic placeholder image">
+                                          <div class="media-body">
+                                            <h5 class="mt-0">Sandra Bullocks</h5>
+                                            <p>sandra@gmail.com</p>
+                                          </div>
+                                        </div>
+                                </div>
+                                </div>
+                                <div class="col-lg-auto col-12">
+                                <div class="people-progress">
+                                    <span>54%</span>
+                                    <div class="progress-people-team">
+                                     <div class="progress-people-main" style="width:54%"></div>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="col-xl-auto col-lg-12 col-sm-12">
+                                   <ul class="ul-people d-flex align-items-center p-0 m-0">
+                                        <li class="favorites-btn">
+                                            
+                                             <div class="add">
+                                               <img src="assets/images/star.svg" class="dark-svg">
+                                            <img src="assets/images/star-dark.svg" class="light-svg">
+                                            <span class="ml-1">Add to favorites</span>
+                                            </div>
+                                            <div class="remove">
+                                                <img src="assets/images/star-fill.svg" class="dark-svg">
+                                                <img src="assets/images/star-fill-dark.svg" class="light-svg">
+                                                <span class="ml-1">Remove favorites</span>
+                                            </div>
+                                           
+                                        </li>
+                                        
+                                        <li class="notification-btn">
+                                            <div class="mute">
+                                                <img src="assets/images/notifications-off.svg" class="dark-svg">
+                                                <img src="assets/images/notifications-dark.svg" class="light-svg">
+                                                <span class="ml-1">Mute notification</span>
+                                            </div>
+                                            <div class="unmute">
+                                                <img src="assets/images/notifications.svg" class="dark-svg">
+                                                <img src="assets/images/notifications-fill-dark.svg" class="light-svg">
+                                                <span class="ml-1">Unmute notification</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                    <div class="coll-grp multiple-layer">
+                        
+                        <div href="#"  class="main-coll">
+                            <div class="row align-items-center">
+                                <div class="col-auto">
+                                <div class="user-detail">
+                                    <div class="media align-items-center">
+                                          <img class="" src="assets/images/user.jpg" alt="Generic placeholder image">
+                                          <div class="media-body">
+                                            <h5 class="mt-0">Andrew Flintoff
+
+</h5>
+                                            <p>andrew941@gmail.com
+
+</p>
+                                          </div>
+                                        </div>
+                                </div>
+                                </div>
+                                <div class="col-lg-auto col-12">
+                                <div class="people-progress">
+                                    <span>72%</span>
+                                    <div class="progress-people-team">
+                                     <div class="progress-people-main" style="width:72%"></div>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="col-xl-auto col-lg-12 col-sm-12">
+                                  <ul class="ul-people d-flex align-items-center p-0 m-0">
+                                        <li class="favorites-btn">
+                                            
+                                             <div class="add">
+                                               <img src="assets/images/star.svg" class="dark-svg">
+                                            <img src="assets/images/star-dark.svg" class="light-svg">
+                                            <span class="ml-1">Add to favorites</span>
+                                            </div>
+                                            <div class="remove">
+                                                <img src="assets/images/star-fill.svg" class="dark-svg">
+                                                <img src="assets/images/star-fill-dark.svg" class="light-svg">
+                                                <span class="ml-1">Remove favorites</span>
+                                            </div>
+                                           
+                                        </li>
+                                        
+                                        <li class="notification-btn">
+                                            <div class="mute">
+                                                <img src="assets/images/notifications-off.svg" class="dark-svg">
+                                                <img src="assets/images/notifications-dark.svg" class="light-svg">
+                                                <span class="ml-1">Mute notification</span>
+                                            </div>
+                                            <div class="unmute">
+                                                <img src="assets/images/notifications.svg" class="dark-svg">
+                                                <img src="assets/images/notifications-fill-dark.svg" class="light-svg">
+                                                <span class="ml-1">Unmute notification</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                    <div class="coll-grp multiple-layer">
+                        
+                        <div href="#"  class="main-coll">
+                            <div class="row align-items-center">
+                                <div class="col-auto">
+                                <div class="user-detail">
+                                    <div class="media align-items-center">
+                                          <img class="" src="assets/images/user-03.jpg" alt="Generic placeholder image">
+                                          <div class="media-body">
+                                            <h5 class="mt-0">Rachel Macadams</h5>
+                                            <p>r.mcadams@gmail.com</p>
+                                          </div>
+                                        </div>
+                                </div>
+                                </div>
+                                <div class="col-lg-auto col-12">
+                                <div class="people-progress">
+                                    <span>63%</span>
+                                    <div class="progress-people-team">
+                                     <div class="progress-people-main" style="width:63%"></div>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="col-xl-auto col-lg-12 col-sm-12">
+                                  <ul class="ul-people d-flex align-items-center p-0 m-0">
+                                        <li class="favorites-btn">
+                                            
+                                             <div class="add">
+                                               <img src="assets/images/star.svg" class="dark-svg">
+                                            <img src="assets/images/star-dark.svg" class="light-svg">
+                                            <span class="ml-1">Add to favorites</span>
+                                            </div>
+                                            <div class="remove">
+                                                <img src="assets/images/star-fill.svg" class="dark-svg">
+                                                <img src="assets/images/star-fill-dark.svg" class="light-svg">
+                                                <span class="ml-1">Remove favorites</span>
+                                            </div>
+                                           
+                                        </li>
+                                        
+                                        <li class="notification-btn">
+                                            <div class="mute">
+                                                <img src="assets/images/notifications-off.svg" class="dark-svg">
+                                                <img src="assets/images/notifications-dark.svg" class="light-svg">
+                                                <span class="ml-1">Mute notification</span>
+                                            </div>
+                                            <div class="unmute">
+                                                <img src="assets/images/notifications.svg" class="dark-svg">
+                                                <img src="assets/images/notifications-fill-dark.svg" class="light-svg">
+                                                <span class="ml-1">Unmute notification</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+ 
+
+<?php include "footer.php" ?>
