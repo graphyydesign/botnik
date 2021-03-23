40ef91aaca6c55198e15a48c2addeee022525b12
graphyydesign <info@graphyy.design>
2021-03-23 23:54:40 +0530

    Updated

--- add-people.php
+++ add-people.php
@@ -25,6 +25,15 @@
                         <div class="col-sm-12 col-11">
                         <div class="edit-profile-box">
                             <form>
+                                
+                             
+                                    
+                                    <div id="dynamic_field">
+                                    
+                             
+                                        
+                                 </div>
+                                
                                 <div class="input-grp">
                                     <label>Name</label>
                                     <div class="input-grp-inner">
@@ -32,7 +41,7 @@
                                 </div>
                                 </div>
                                 <div class="input-grp">
-                                    <label>Edit Email address</label>
+                                    <label>Email address</label>
                                     <div class="input-grp-inner">
                                     <input type="email" class="input-class" placeholder="Enter an email address" value="">
                                 </div>
@@ -42,7 +51,7 @@
                                 <div class="input-grp mb-0">
                                 <div class="btn-grp">
                                     <div class="text-left">
-                                <button class="">Add another person</button>
+                                    <button class="" type="button" name="add" id="add">Add another person</button>
                                     </div>
                                      </div>
                                      </div>
@@ -55,7 +64,7 @@
                 <div class="past-section">
             
                     <div class="row justify-content-center">
-                        <div class="col-12">
+                          <div class="col-sm-12 col-11">
                         <div class="edit-profile-box">
                             <form>
                                 <div class="input-grp">
@@ -86,6 +95,22 @@
         </div>
     </section>
 
- 
+    <script type="text/javascript">
+           $(document).ready(function() {
+               var i = 1;
+
+               $('#add').click(function() {
+                   i++;
+                   $('#dynamic_field').append('<div id="row-class'+i+'" class="input-grp"> <label class="mb-0">Inviting</label> <div class="input-grp-inner"> <div class="coll-grp multiple-layer"> <div class="main-coll"> <div class="row align-items-center position-relative"> <div class="col-auto"> <div class="user-detail"> <div class="media align-items-center"> <img class="" src="assets/images/tim.png" alt="Generic placeholder image"> <div class="media-body"> <h5 class="mt-0">Tim Chilcott</h5> <p>tim.chilcott@botnik.com</p> </div> </div> </div> </div> <div class="dropdown-custom"> <div class="dropdown dropdown-user"> <div class="dropdown-toggle p-0 " href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <div class="nav-bar-02"> <img src="assets/images/dots-bar.svg" class="dark-svg"> <img src="assets/images/dots-bar-light.svg" class="light-svg"> </div> </div> <div class="dropdown-menu" aria-labelledby="navbarDropdown3"> <a class="dropdown-item btn_remove" href="javascript:void(0)" id="' + i + '" name="remove"> <img src="assets/images/circle-outline.svg" class="dark-svg"> <img src="assets/images/dark-circle-outline.svg" class="light-svg"> Remove person </a> </div> </div> </div> </div> </div> </div> </div> </div>');
+               });
+
+               $(document).on('click', '.btn_remove', function() {
+                   var button_id = $(this).attr("id");
+                   $('#row-class' + button_id + '').remove();
+               });
+
+           });
+       </script>
+
 
 <?php include "footer.php" ?>
